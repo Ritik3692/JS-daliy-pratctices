3c907d5209cb2cf9aad6688eb16359177ead4054
Ritik3692 <ritikkashyap464@gmail.com>
2024-09-27 18:03:15 +0530

    new day

--- day-4/app.js
+++ day-4/app.js
@@ -13,3 +13,23 @@ console.log(NaN === NaN)
 
 console.log(1<2<3)
 console.log(3>2>1)
+
+
+
+
+let arr= [1,2,3,4,5,6,7,8,8,8,8,6,4,3,22,2,22,22,22,22,22,22]
+
+let r = arr.filter((e)=> e<22).map((c) => c*2);
+console.log(r);
+
+
+let tar=22
+let re = [];
+arr.forEach((ele,index) => {
+    
+    if(ele === tar){
+        re.push(index);
+    }
+    
+});
+console.log(re)
