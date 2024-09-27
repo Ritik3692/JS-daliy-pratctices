38c07fe608f39ffc335b1b8c8dc12900e7d0fba2
Ritik3692 <ritikkashyap464@gmail.com>
2024-09-27 18:01:27 +0530

    new day

---
+++ day-4/app.js
@@ -0,0 +1,10 @@
+console.log('5' -  1);    
+// output:- 4
+console.log(  5 + '5');
+// output:- 55
+console.log('5' /  2);
+// output:- 52 // output:- 2.5
+console.log(  5 * '5');
+// output:-55 // output:-25
+console.log((5  +'5') * 7);
+// output:- 55
